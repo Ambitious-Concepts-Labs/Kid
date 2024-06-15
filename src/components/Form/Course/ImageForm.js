b3fda36847f599210fc1e493531cb2ea37418fcb
Dominique Hosea <dominique11h@yahoo.com>
2024-06-15 02:21:00 -0500

    fix(*): update course image form

--- src/components/Form/Course/ImageForm.js
+++ src/components/Form/Course/ImageForm.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import CloudinaryUploadWidget from "../../Image/CloudinaryUploadWidget";
-import { Cloudinary } from "@cloudinary/url-gen";
-import { AdvancedImage, responsive, placeholder } from "@cloudinary/react";
 
 const ImageForm = ({ initialData, courseId }) => {
   const [publicId, setPublicId] = useState(process.env.REACT_APP_CLOUD_API_KEY);
   const [cloudName] = useState(process.env.REACT_APP_CLOUND_NAME);
-  const [uploadPreset] = useState(process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET);
+  const [uploadPreset] = useState(
+    process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
+  );
   const [uwConfig] = useState({
     cloudName,
     uploadPreset,
@@ -18,23 +18,16 @@ const ImageForm = ({ initialData, courseId }) => {
     // tags: ["users", "profile"], //add the given tags to the uploaded files
     // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
     // clientAllowedFormats: ["images"], //restrict uploading to image files only
-    maxImageFileSize: 2000000,  //restrict file size to less than 2MB
+    maxImageFileSize: 2000000, //restrict file size to less than 2MB
     maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
     // theme: "purple", //change to a purple theme
   });
-  const cld = new Cloudinary({
-    cloud: {
-      cloudName,
-    },
-  });
-  const myImage = cld.image(publicId);
 
   const [isEditing, setIsEditing] = useState(false);
   const [imageUrl, setImageUrl] = useState(initialData?.imageUrl || "");
 
   const toggleEdit = () => setIsEditing((current) => !current);
 
-  console.log(myImage);
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
@@ -66,12 +59,13 @@ const ImageForm = ({ initialData, courseId }) => {
         !imageUrl ? (
           <div className="flex items-center justify-center bg-slate-200 rounded-md h-60">
             <span className="text-slate-500 text-2xl">🖼️</span>
+            <h2>Noooo</h2>
           </div>
         ) : (
           <div className="relative aspect-video mt-2">
-            <AdvancedImage
-              cldImg={myImage}
-              plugins={[responsive(), placeholder("blur")]}
+            <img
+              src={imageUrl}
+              alt="Course"
               className="object-cover rounded-md"
               style={{ width: "100%", height: "auto" }}
             />
