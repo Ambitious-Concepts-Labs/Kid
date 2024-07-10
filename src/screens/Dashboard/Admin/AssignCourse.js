8dbab3085611be1576f719984141b56b9fd4b0bc
Dominique Hosea <dominique11h@yahoo.com>
2024-07-10 11:41:31 -0500

    fix(*): implemented lazy loading on assign course

--- src/screens/Dashboard/Admin/AssignCourse.js
+++ src/screens/Dashboard/Admin/AssignCourse.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
-import imgPlaceholder from "../image-placeholder.png";
+import React, { useState, useEffect, Suspense, lazy } from "react";
 import { assignCourse, selectCourse } from "../../../utils/courseFunctions";
 import Layout from "../../../components/Dashboard/Layout";
 import { useNavigate } from "react-router-dom";
 import useGetAllCourses from "../../../hooks/useGetAllCourses";
 import useGetAllUsers from "../../../hooks/useGetAllUsers";
+
+const AssignCourseForm = lazy(() => import("../../../components/Admin/AssignCourseForm"));
+const CourseDetails = lazy(() => import("../../../components/Admin/CourseDetails"));
 
 const AssignCourse = (props) => {
   const { currentUser } = props;
@@ -143,209 +145,48 @@ const AssignCourse = (props) => {
   if (error) return <div>Error: {error.message}</div>;
   if (!currentUser) return <h1>Loading User...</h1>;
   if (!currentUser.isAdmin) history("/dashboard");
-    return (
-      <Layout>
-        <div id="assign-course" className="w-1/2 mx-auto">
-          <div className="form-group mb-4">
-            <label
-              htmlFor="assigned-user"
-              className="block text-sm font-medium text-gray-700"
-            >
-              User
-            </label>
-            <input
-              type="text"
-              className="form-control block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              id="assigned-user"
-              onClick={(e) => {
-                setShowUserResults(true);
-                setFocused(e.target.id);
-                if (!e.target.value.length) {
-                  setFilteredUsers(users);
-                }
-              }}
-              onChange={(e) => {
-                handleAssignedUser(e);
-              }}
+  return (
+    <Layout>
+      <div id="assign-course" className="w-1/2 mx-auto">
+        <Suspense fallback={<div>Loading form...</div>}>
+          <AssignCourseForm
+            users={users}
+            courses={courses}
+            handleAssignedUser={handleAssignedUser}
+            handleCourseName={handleCourseName}
+            handleSelectedUser={handleSelectedUser}
+            handleSelectedCourse={handleSelectedCourse}
+            setShowUserResults={setShowUserResults}
+            setFocused={setFocused}
+            setFilteredUsers={setFilteredUsers}
+            filteredUsers={filteredUsers}
+            showUserResults={showUserResults}
+            isUserFound={isUserFound}
+            isCourseFound={isCourseFound}
+            setShowCourseResults={setShowCourseResults}
+            filteredCourses={filteredCourses}
+            showCourseResults={showCourseResults}
+          />
+        </Suspense>
+
+        {isCourseFound && (
+          <Suspense fallback={<div>Loading course details...</div>}>
+            <CourseDetails
+              assignedCourse={assignedCourse}
+              isAssignedCourseLoading={isAssignedCourseLoading}
+              setAssignedCourse={setAssignedCourse}
+              setIsCourseFound={setIsCourseFound}
+              assignCourse={assignCourse}
+              isUserFound={isUserFound}
+              selectedUser={selectedUser}
+              currentUser={currentUser}
+              setIsUserFound={setIsUserFound}
             />
-            {showUserResults && (
-              <div id="results-container" className="mt-2 space-y-2">
-                {filteredUsers.slice(0, 10).map((user) => {
-                  return (
-                    <button
-                      key={user._id}
-                      type="button"
-                      className="form-control block w-full text-left border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                      onClick={() => {
-                        handleSelectedUser(user);
-                      }}
-                    >
-                      {user.username}
-                    </button>
-                  );
-                })}
-              </div>
-            )}
-          </div>
-
-          <div
-            className={`form-group mb-4 ${isUserFound ? "block" : "hidden"}`}
-          >
-            <label
-              htmlFor="assigned-course-name"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Course
-            </label>
-            {!isCourseFound ? (
-              <input
-                type="text"
-                className="form-control block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                id="assigned-course-name"
-                onClick={(e) => {
-                  setShowCourseResults(true);
-                  setFocused(e.target.id);
-                }}
-                onChange={(e) => {
-                  handleCourseName(e);
-                }}
-              />
-            ) : (
-              <input
-                type="text"
-                className="form-control block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                id="assigned-course-name"
-                disabled
-              />
-            )}
-
-            {showCourseResults && (
-              <div id="results-container" className="mt-2 space-y-2">
-                {courses.map((course) => {
-                  return (
-                    <button
-                      key={course._id}
-                      type="button"
-                      className="form-control block w-full text-left border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                      onClick={() => {
-                        handleSelectedCourse(course);
-                      }}
-                    >
-                      {`Name: ${course.courseName}`}
-                    </button>
-                  );
-                })}
-                {filteredCourses.slice(0, 10).map((course) => {
-                  return (
-                    <button
-                      key={course._id}
-                      type="button"
-                      className="form-control block w-full text-left border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                      onClick={() => {
-                        handleSelectedCourse(course);
-                      }}
-                    >
-                      {`Name: ${course.course_name}, Instructor: ${course.instructor.username}`}
-                    </button>
-                  );
-                })}
-              </div>
-            )}
-          </div>
-
-          {isCourseFound &&
-            (!isAssignedCourseLoading ? (
-              <div className="flex justify-center mt-4 w-full">
-                <div className="card p-4 bg-white shadow-md rounded-lg w-full">
-                  <div className="about-product text-center mt-2">
-                    <img
-                      src={imgPlaceholder}
-                      width="100"
-                      alt="Course"
-                      className="mx-auto"
-                    />
-                    <div className="mt-2">
-                      <h4 className="text-lg font-semibold">
-                        Course Name: {assignedCourse.course_name}
-                      </h4>
-                      <h6 className="mt-1 text-gray-600">
-                        by: {assignedCourse.instructor.username}
-                      </h6>
-                    </div>
-                  </div>
-                  <div className="stats mt-4">
-                    <div className="flex justify-between p-2">
-                      <span className="text-gray-700">Class Number</span>
-                      <span className="text-gray-900">
-                        {assignedCourse.class_number}
-                      </span>
-                    </div>
-                    <div className="flex justify-between p-2">
-                      <span className="text-gray-700">Subject</span>
-                      <span className="text-gray-900">
-                        {assignedCourse.subject}
-                      </span>
-                    </div>
-                    <div className="flex justify-between p-2">
-                      <span className="text-gray-700">Grade Level</span>
-                      <span className="text-gray-900">
-                        {assignedCourse.grade_level}
-                      </span>
-                    </div>
-                    <div className="flex justify-between p-2">
-                      <span className="text-gray-700">
-                        Number of Students Enrolled
-                      </span>
-                      <span className="text-gray-900">
-                        {assignedCourse.num_of_students}
-                      </span>
-                    </div>
-                  </div>
-
-                  <div className="flex justify-center mt-4">
-                    <button
-                      type="button"
-                      className="btn btn-danger w-36 mr-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-                      onClick={() => {
-                        setAssignedCourse({
-                          assignedUser: selectedUser,
-                        });
-                        setIsCourseFound(false);
-                        document.getElementById("assigned-course-name").value =
-                          "";
-                      }}
-                    >
-                      Change Course
-                    </button>
-
-                    <button
-                      type="button"
-                      className="btn btn-primary w-36 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      onClick={() => {
-                        assignCourse({
-                          isUserFound,
-                          currentUser,
-                          assignedCourse,
-                          setIsUserFound,
-                          setIsCourseFound,
-                        });
-                      }}
-                    >
-                      Assign
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ) : (
-              <div className="flex justify-center mt-4">
-                <div className="spinner-border text-indigo-600" role="status">
-                  <span className="visually-hidden">Loading...</span>
-                </div>
-              </div>
-            ))}
-        </div>
-      </Layout>
-    );
+          </Suspense>
+        )}
+      </div>
+    </Layout>
+  );
 };
 
 export default AssignCourse;
